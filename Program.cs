01d5e64f47b8861f5f975881be65c3771eb87023
EstefaniSoto <eri022004@hotmail.com>
2024-06-11 00:38:39 -0400

    Proyecto finalizado

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using BlazingPizza.Data;
+using BlazingPizza.Services;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
+builder.Services.AddScoped<OrderState>();
+
 
 var app = builder.Build();
 
@@ -15,6 +23,17 @@ app.UseRouting();
 
 app.MapRazorPages();
 app.MapBlazorHub();
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
 app.MapFallbackToPage("/_Host");
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
 
 app.Run();
